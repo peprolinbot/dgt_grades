de278ad9234ae14603df5fc18b4997e481cd0e73
Pedro Rey Anca <personal@peprolinbot.com>
2026-04-07 13:56:44 +0200

    Improve multiple exam types support

--- dgt_grade_checker.py
+++ dgt_grade_checker.py
@@ -2,21 +2,71 @@ import requests
 from bs4 import BeautifulSoup
 import argparse
 from datetime import datetime
+import warnings
 
 
-class GradeResponse():
-    def __init__(self, calification: str, num_errors: int, errors: dict = None):
+class BasicGradeResponse:
+    def __init__(self, type: str, calification: str):
         self.calification: str = calification
+        self.type = type
+
+        self.passed: bool = calification == "APTO"
+
+    def __repr__(self) -> str:
+        return f"Type: {self.type}\nCalification: {self.calification}"
+
+
+class TheoryGradeResponse(BasicGradeResponse):
+    def __init__(self, type: str, calification: str, num_errors: int):
+        super().__init__(type, calification)
+
+        self.num_errors: int = num_errors
+
+    def __repr__(self) -> str:
+        lines = [super().__repr__()]
+        lines.append(f"Errors: {self.num_errors}")
+        return "\n".join(lines)
+
+
+class CirculationGradeResponse(BasicGradeResponse):
+    def __init__(
+        self, type: str, calification: str, num_errors: int, errors: dict = {}
+    ):
+        super().__init__(type, calification)
+
         self.num_errors: int = num_errors
         self.errors: dict = errors
 
-        self.passed: bool = (calification == "APTO")
+    def __repr__(self) -> str:
+        lines = [super().__repr__()]
+        lines.append(f"{self.num_errors} Errors:")
+        for err_type, errors in self.errors:
+            lines.append(f"\t- {err_type}: {', '.join(errors)}")
+        return "\n".join(lines)
+
+
+class ClosedCircuitGradeResponse(BasicGradeResponse):
+    def __init__(
+        self, type: str, calification: str, num_errors: int, maneuvers: dict = {}
+    ):
+        super().__init__(type, calification)
+
+        self.maneuvers: dict = maneuvers
 
     def __repr__(self) -> str:
-        return f"Calification: {self.calification} | {self.num_errors} Errors{f': {self.errors}' if not self.errors is None else ''}"
+        lines = [super().__repr__()]
+
+        lines.append("Errors:")
+        lines.append("| Maniobra | Eliminatorias | Deficientes | Leves |")
+        lines.append("|------------------------------------------------|")
+        fmt = "| {:^8} | {:^13} | {:^11} | {:^5} |"
+        for name, errors in self.maneuvers.items():
+            lines.append(fmt.format(*([name] + list(errors.values()))))
+
+        return "\n".join(lines)
 
 
-class DGTGradeChecker():
+class DGTGradeChecker:
     def __init__(self):
         self.url = "https://sedeclave.dgt.gob.es/WEB_NOTP_CONSULTA/consultaNota.faces"
         self.session = requests.Session()
@@ -24,8 +74,8 @@ class DGTGradeChecker():
     def _get_view_state(self) -> str:
         response = self.session.get(self.url)
         response.raise_for_status()
-        soup = BeautifulSoup(response.text, 'html.parser')
-        return soup.find('input', {'name': 'javax.faces.ViewState'})['value']
+        soup = BeautifulSoup(response.text, "html.parser")
+        return soup.find("input", {"name": "javax.faces.ViewState"})["value"]
 
     def _submit_form(self, payload: dict) -> BeautifulSoup:
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
@@ -33,7 +83,9 @@ class DGTGradeChecker():
         response.raise_for_status()
         return BeautifulSoup(response.content, "html.parser")
 
-    def fetch_grade(self, dni: str, exam_date: datetime, licenseClass: str, birth_date: datetime) -> GradeResponse:
+    def fetch_grade(
+        self, dni: str, exam_date: datetime, licenseClass: str, birth_date: datetime
+    ) -> BasicGradeResponse:
         view_state = self._get_view_state()
         payload = {
             "formularioBusquedaNotas": "formularioBusquedaNotas",
@@ -48,54 +100,102 @@ class DGTGradeChecker():
         soup = self._submit_form(payload)
 
         # Find all <li> elements with the class 'msgError'
-        error_messages = soup.find_all('li', class_='msgError')
+        error_messages = soup.find_all("li", class_="msgError")
         if not error_messages:
             calificacion = soup.find(
-                id="formularioResultadoNotas:j_id38:0:j_id70").get_text(strip=True)
+                id="formularioResultadoNotas:j_id38:0:j_id70"
+            ).get_text(strip=True)
 
             tipo_examen = soup.find(
-                id="formularioResultadoNotas:j_id38:0:j_id54").get_text(strip=True)
+                id="formularioResultadoNotas:j_id38:0:j_id54"
+            ).get_text(strip=True)
 
             if tipo_examen == "CIRCULACIÓN":
-                errores = {}
-                num_errores = 0
+                errors = {}
+                num_errors = 0
                 for tipo in ("eliminatoria", "deficiente", "leve"):
-                    errores_text = soup.find(
-                        'td', headers=f"faltasCometidas {tipo}").get_text(strip=True)
+                    type_errors_text = soup.find(
+                        "td", headers=f"faltasCometidas {tipo}"
+                    ).get_text(strip=True)
 
-                    if errores_text == "Detalle no disponible" or errores_text == "0":
-                        errores_tipo = []
+                    if (
+                        type_errors_text == "Detalle no disponible"
+                        or type_errors_text == "0"
+                    ):
+                        type_errors = []
                     else:
-                        errores_tipo = errores_text.split(" - ")
-                        num_errores += len(errores_tipo)
+                        type_errors = type_errors_text.split(" - ")
+                        num_errors += len(type_errors)
 
                     # We want the key to be in plural
-                    errores[tipo+"s"] = errores_tipo
+                    errors[tipo + "s"] = type_errors
 
-            else:  # Idk which other types there are apart from CIRCULACIÓN and TEÓRICO COMÚN, so we default to TEÓRICO COMÚN
-                num_errores = int(soup.find(
-                    id="formularioResultadoNotas:j_id38:0:j_id78").text)
-                errores = None
+                return CirculationGradeResponse(
+                    type=tipo_examen,
+                    calification=calificacion,
+                    num_errors=num_errors,
+                    errors=errors,
+                )
+            elif tipo_examen == "DESTREZA EN CIRCUITO CERRADO":
+                tabla = soup.find(id="formularioResultadoNotas:j_id38:0:tablaRegistros")
+                maneuvers = {}
+                num_errors = 0
+                for maniobra in tabla.find_all("tbody"):
+                    maneuver_name = maniobra.find(
+                        "td", headers="faltasCometidas maniobra"
+                    ).get_text(strip=True)
+                    maneuvers[maneuver_name] = {}
+                    for tipo in ("eliminatoria", "deficiente", "leve"):
+                        num_errors_text = maniobra.find(
+                            "td", headers=f"faltasCometidas {tipo}"
+                        ).get_text(strip=True)
 
-            return GradeResponse(calification=calificacion, num_errors=num_errores, errors=errores)
+                        maneuvers[maneuver_name][tipo + "s"] = int(num_errors_text)
+                return ClosedCircuitGradeResponse(
+                    type=tipo_examen,
+                    calification=calificacion,
+                    num_errors=num_errors,
+                    maneuvers=maneuvers,
+                )
+            elif "TEÓRICO" in tipo_examen:
+                num_errors = int(
+                    soup.find(id="formularioResultadoNotas:j_id38:0:j_id78").text
+                )
+                return TheoryGradeResponse(
+                    type=tipo_examen,
+                    calification=calificacion,
+                    num_errors=num_errors,
+                )
+            else:
+                warnings.warn(f"Unknown exam type: {tipo_examen}")
+                return BasicGradeResponse(type=tipo_examen, calification=calificacion)
         else:
             # Extract the text from the errors and join with newlines
-            error_messages_text = '\n'.join(li.get_text(strip=True)
-                                            for li in error_messages)
+            error_messages_text = "\n".join(
+                li.get_text(strip=True) for li in error_messages
+            )
 
             raise ValueError(error_messages_text)
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Check the grade of a DGT test you did. Uses https://sedeclave.dgt.gob.es/WEB_NOTP_CONSULTA/resultadoConsultaNota.faces")
+        description="Check the grade of a DGT test you did. Uses https://sedeclave.dgt.gob.es/WEB_NOTP_CONSULTA/resultadoConsultaNota.faces"
+    )
     parser.add_argument("DNI", type=str, help="Your DNI (NIF/NIE)")
-    parser.add_argument("exam_date", type=str,
-                        help="The date when you took the exam, in DD/MM/YYYY format")
-    parser.add_argument("license_class", type=str,
-                        help="The type of driving license the test was for, e.g. B")
-    parser.add_argument("birth_date", type=str,
-                        help="Your birth date, in DD/MM/YYYY format")
+    parser.add_argument(
+        "exam_date",
+        type=str,
+        help="The date when you took the exam, in DD/MM/YYYY format",
+    )
+    parser.add_argument(
+        "license_class",
+        type=str,
+        help="The type of driving license the test was for, e.g. B",
+    )
+    parser.add_argument(
+        "birth_date", type=str, help="Your birth date, in DD/MM/YYYY format"
+    )
 
     args = parser.parse_args()
 
@@ -107,7 +207,8 @@ def main():
 
     dgt_grade_checker = DGTGradeChecker()
     grade = dgt_grade_checker.fetch_grade(
-        args.DNI, exam_date, args.license_class, birth_date)
+        args.DNI, exam_date, args.license_class, birth_date
+    )
 
     print(grade)
 
